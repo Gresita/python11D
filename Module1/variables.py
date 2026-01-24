328cd28f30d53dd115132d64d0c69497968b1dcb
ArianitaGashi <arianita.gashi@digitalschool.tech>
2026-01-24 12:40:39 +0100

    module1

---
+++ Module1/variables.py
@@ -0,0 +1,72 @@
+'''
+temperature = 96.8
+
+name = "Erina"
+
+mosha = 30
+
+print(type(temperature))
+
+print(type(name))
+
+print(type(mosha))
+'''
+
+#Kalkulime
+
+x = 8
+
+y = 10
+
+result = x+y
+
+print(result)
+
+#update values
+
+age = 30
+
+# age = age + 1
+
+age+=1
+
+print(age)
+
+#combine values
+
+first_name = "Arianita"
+last_name = "Gashi"
+
+full_name = first_name +" "+last_name
+
+print(full_name)
+
+#array (lists)
+
+fav_colors = ["red", "green", "blue", "yellow", "purple"]
+first = fav_colors[0]
+second = fav_colors[1]
+
+print(first)
+print(second)
+
+#methods for list
+#append - add an item at the end of the list
+fav_colors.append("orange")
+print(fav_colors)
+
+#insert - add element in a specifik index
+fav_colors.insert(2, "white")
+print(fav_colors)
+
+#remove
+fav_colors.remove("blue")
+
+del fav_colors[4]
+
+print(fav_colors)
+
+#update
+
+fav_colors[0] = "pink"
+print(fav_colors)
